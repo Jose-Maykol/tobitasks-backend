6e9883aba9f3f3dcdbff38e48d10f675e1f40ae4
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-03-13 23:16:23 -0500

    feat: implement user authentication in project creation

--- src/modules/v1/projects/controllers/project.controller.ts
+++ src/modules/v1/projects/controllers/project.controller.ts
@@ -1,18 +1,25 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common'
 import { CreateProjectDto } from '../dtos/create-project.dto'
 import { ProjectsService } from '../services/projects.service'
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard'
+import { JwtPayload } from '../../auth/interfaces/jwt-payload'
 
 @Controller()
 export class ProjectsController {
 	constructor(private readonly projectService: ProjectsService) {}
 
 	@Post()
-	create(@Body() body: CreateProjectDto) {
+	@UseGuards(JwtAuthGuard)
+	create(
+		@Req() req: Request & { user: JwtPayload },
+		@Body() body: CreateProjectDto
+	) {
+		const { sub: userId } = req.user
 		const { name, description } = body
-
 		const project = this.projectService.create({
 			name,
-			description
+			description,
+			userId
 		})
 
 		return project
--- src/modules/v1/projects/services/projects.service.ts
+++ src/modules/v1/projects/services/projects.service.ts
@@ -1,19 +1,35 @@
-import { ConflictException, Injectable } from '@nestjs/common'
+import { ConflictException, Injectable, Logger } from '@nestjs/common'
+import { ObjectId } from 'mongodb'
 import { CreateProjectDto } from '../dtos/create-project.dto'
 import { ProjectRepository } from '../../database/repositories/project.repository'
 
 @Injectable()
 export class ProjectsService {
+	private logger = new Logger('ProjectsService')
+
 	constructor(private readonly projectRepository: ProjectRepository) {}
 
-	async create(data: CreateProjectDto) {
+	async create(data: CreateProjectDto & { userId: string }) {
 		const { name, description } = data
 
 		const project = await this.projectRepository.findByName(name)
 
 		if (project) throw new ConflictException('Project already exists')
 
-		return this.projectRepository.create({ name, description })
+		const DEFAULT_STAGES = [
+			{ name: 'En planificación', order: 0, color: 'neutral-600' },
+			{ name: 'En progreso', order: 1, color: 'blue-600' },
+			{ name: 'Hecho', order: 2, color: 'green-600' }
+		]
+
+		this.logger.log(`Creating project ${name}`)
+
+		return this.projectRepository.create({
+			name,
+			description,
+			stages: DEFAULT_STAGES,
+			createdBy: new ObjectId(data.userId)
+		})
 	}
 
 	async findAll() {
